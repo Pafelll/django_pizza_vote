5322eaf78c21f8477679e2f142471662fe533bf9
Pafel <pawel.halasa95@gmail.com>
2020-02-27 18:35:00 +0100

    Added test

--- pizza_vote/tests.py
+++ pizza_vote/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from pizza_vote.models import Vote, Pizza, Topping
 
-# Create your tests here.
+
+class ToppingTest(TestCase):
+    def create_topping(self, name='Jalapeno'):
+        return Topping.objects.create(name=name)
+
+    def test_create_topping(self):
+        t = self.create_topping()
+        self.assertTrue(isinstance(t, Topping))
+        self.assertEqual(t.__str__(), t.name)
+
